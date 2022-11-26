4ea5d063ca345e59bf72c709f42d68d02e6d3adb
i-muiri <ianmuiri99@gmail.com>
2022-11-26 10:15:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+var num1=31;
+var num2=2;
+const multiply = num1 * num2;
+
+let random = 10;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = 20;
